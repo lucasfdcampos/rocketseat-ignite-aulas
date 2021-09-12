06368d43d932001f1bd68a7728e4b72a7f133d6d
Lucas Campos <lucascampos.ads@gmail.com>
2021-09-12 09:25:24 -0300

    feat: Add Specification classes

--- nodejs/rentalx/src/modules/cars/repositories/CategoriesRepository.ts
+++ nodejs/rentalx/src/modules/cars/repositories/CategoriesRepository.ts
@@ -11,21 +11,22 @@ class CategoriesRepository implements ICategoriesRepository {
     this.categories = [];
   }
 
-  create({ name, description }: ICreateCategoryDTO): void {
-    const category = new Category();
-    Object.assign(category, { name, description, created_at: new Date() });
+  findByName(name: string): Category {
+    const category = this.categories.find(category => category.name === name);
 
-    this.categories.push(category);
+    return category;
   }
 
   list(): Category[] {
     return this.categories;
   }
 
-  findByName(name: string): Category {
-    const category = this.categories.find(category => category.name === name);
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
 
-    return category;
+    Object.assign(category, { name, description, created_at: new Date() });
+
+    this.categories.push(category);
   }
 }
 
--- nodejs/rentalx/src/modules/cars/services/CreateCategoryService.ts
+++ nodejs/rentalx/src/modules/cars/services/CreateCategoryService.ts
@@ -12,7 +12,7 @@ class CreateCategoryService {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
 
     if (categoryAlreadyExists) {
-      throw new Error('Category already exists');
+      throw new Error('Category already exists.');
     }
 
     this.categoriesRepository.create({ name, description });
