e00b9917c0c7156cb07f69148d7d89f7d87b8d82
Lucas Campos <lucascampos.ads@gmail.com>
2021-10-07 22:47:57 -0300

    feat: Add class AppError and middleware to show errors

--- nodejs/rentalx/src/middlewares/ensureAuthenticated.ts
+++ nodejs/rentalx/src/middlewares/ensureAuthenticated.ts
@@ -1,42 +1,44 @@
 import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
+import { AppError } from '../errors/AppError';
 import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
 
-interface IPayload {
+interface IPayLoad {
+  iat: number;
+  exp: number;
   sub: string;
 }
 
-export async function ensureAuthenticated(
+async function ensureAuthenticated(
   request: Request,
   response: Response,
   next: NextFunction,
-): Promise<string> {
+): Promise<void> {
   const authHeader = request.headers.authorization;
 
   if (!authHeader) {
-    throw new Error('Token missing');
+    throw new AppError('Token missing', 401);
   }
 
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(
-      token,
-      '87c3743276065d8ddd490714c0e389e2',
-    ) as IPayload;
+    const decoded = verify(token, '87c3743276065d8ddd490714c0e389e2');
+
+    const { sub: user_id } = decoded as IPayLoad;
 
     const usersRepository = new UsersRepository();
-    const user = usersRepository.findById(user_id);
+    const user = await usersRepository.findById(user_id);
 
     if (!user) {
-      throw new Error('User does not exists');
+      throw new AppError('User does not exists', 401);
     }
 
     next();
   } catch {
-    throw new Error('Token invalid');
+    throw new AppError('Token JWT invalid', 401);
   }
-
-  return token;
 }
+
+export { ensureAuthenticated };
