8a7f841703c65b9177159910de7c907e79415c33
Lucas Campos <lucascampos.ads@gmail.com>
2021-11-02 18:55:05 -0300

    feat: Add new unit tests car

--- nodejs/rentalx/src/modules/cars/infra/typeorm/entities/Car.ts
+++ nodejs/rentalx/src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,23 +1,43 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+@Entity('cars')
 class Car {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
-  available: boolean;
+  @Column()
+  available = true;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
 
   category_id: string;
 
   created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.available = true;
+      this.created_at = new Date();
+    }
+  }
 }
 
 export { Car };
