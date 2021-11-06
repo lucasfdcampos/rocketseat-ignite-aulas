def5a6b137d0dc77662d02a19903c0e88218fe66
Lucas Campos <lucascampos.ads@gmail.com>
2021-11-06 17:59:50 -0300

    feat: Add ensureAdmin middleware

--- nodejs/rentalx/src/shared/infra/typeorm/index.ts
+++ nodejs/rentalx/src/shared/infra/typeorm/index.ts
@@ -1,14 +1,25 @@
-import { createConnection, getConnectionOptions } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
 interface IOptions {
   host: string;
 }
 
-getConnectionOptions().then(options => {
-  const newOptions = options as IOptions;
-  // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
-  newOptions.host = 'database';
-  createConnection({
-    ...options,
-  });
-});
+// getConnectionOptions().then(options => {
+//   const newOptions = options as IOptions;
+//   // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+//   newOptions.host = 'database';
+//   createConnection({
+//     ...options,
+//   });
+// });
+
+export default async (host = 'database'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+      host,
+    }),
+  );
+};
