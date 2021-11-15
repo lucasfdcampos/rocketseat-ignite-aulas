0d2d598fde8a04ceb9a65610085cce32c05e4978
Lucas Campos <lucascampos.ads@gmail.com>
2021-11-15 20:19:51 -0300

    feat: Add car specification useCase

--- nodejs/rentalx/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
+++ nodejs/rentalx/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -26,10 +26,21 @@ class SpecificationsRepository implements ISpecificationsRepository {
     return specifications;
   }
 
-  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+  async create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification> {
     const specification = this.repository.create({ name, description });
 
     await this.repository.save(specification);
+
+    return specification;
+  }
+
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const specifications = await this.repository.findByIds(ids);
+
+    return specifications;
   }
 }
 
--- nodejs/rentalx/src/modules/cars/repositories/ISpecificationsRepository.ts
+++ nodejs/rentalx/src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../entities/Specification';
+import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 
 interface ICreateSpecificationDTO {
   name: string;
@@ -7,8 +7,12 @@ interface ICreateSpecificationDTO {
 
 interface ISpecificationsRepository {
   findByName(name: string): Promise<Specification>;
+  findByIds(ids: string[]): Promise<Specification[]>;
   list(): Promise<Specification[]>;
-  create({ name, description }: ICreateSpecificationDTO): Promise<void>;
+  create({
+    name,
+    description,
+  }: ICreateSpecificationDTO): Promise<Specification>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
