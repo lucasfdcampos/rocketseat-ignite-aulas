863af2fc3dfdacd66105c55eb8a01d43b03c581e
lucas.campos <lucas.campos@mksolutions.com.br>
2022-09-22 22:08:57 -0300

    add header

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,13 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post"
+
+import './global.css';
 
 export function App() {
   return (
     <>
+      <Header />
+
       <Post 
         author="Lucas Campos" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dignissimos in quo quos necessitatibus atque, assumenda fugiat architecto placeat iure eveniet reprehenderit unde mollitia? Ad nostrum minus corporis explicabo reprehenderit!"
