897c2c59ef19382cb42c8319256c214a7bb53572
lucas.campos <lucas.campos@mksolutions.com.br>
2022-09-25 15:08:58 -0300

    finish sidebar

--- reactjs/01-fundamentos-reactjs/src/App.jsx
+++ reactjs/01-fundamentos-reactjs/src/App.jsx
@@ -1,22 +1,29 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post"
 
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
   return (
-    <>
+    <div>
       <Header />
 
-      <Post 
-        author="Lucas Campos" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dignissimos in quo quos necessitatibus atque, assumenda fugiat architecto placeat iure eveniet reprehenderit unde mollitia? Ad nostrum minus corporis explicabo reprehenderit!"
-      />
-       <Post 
-        author="John Doe" 
-        content="Novo post"
-      />
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Lucas Campos" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis dignissimos in quo quos necessitatibus atque, assumenda fugiat architecto placeat iure eveniet reprehenderit unde mollitia? Ad nostrum minus corporis explicabo reprehenderit!"
+          />
+          <Post 
+            author="John Doe" 
+            content="Novo post"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
 
---
+++ reactjs/01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { PencilSimpleLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/lucasfdcampos.png" />
+
+        <strong>Lucas Campos</strong>
+        <span>Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
